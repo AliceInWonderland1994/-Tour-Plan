b5fa442f84d2738a1c91b30202a08ddb0074160f
Alise <alicemint@mail.ru>
2020-07-21 17:21:47 +0400

    create: added swiper-sloder in hotel section

--- js/main.js
+++ js/main.js
@@ -4,7 +4,8 @@ var mySwiper = new Swiper('.swiper-container', {
   
     // Navigation arrows
     navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
+      nextEl: '.slider-button--next',
+      prevEl: '.slider-button--prev',
     },
+    effect: "fade"
   })
