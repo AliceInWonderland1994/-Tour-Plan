749f34aac0207af1875b45c1d32a30e32a499d1f
Alise <alicemint@mail.ru>
2020-07-28 16:42:56 +0400

    create: mobile menu

--- js/main.js
+++ js/main.js
@@ -1,48 +1,53 @@
-var hotelSlider = new Swiper('.hotel-slider', {
-    // Optional parameters
-    loop: true,
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.hotel-slider__button--next',
-      prevEl: '.hotel-slider__button--prev',
-    },
-    effect: 'cube',
-    keyboard: true,
-});
-
-var reviewsSlider = new Swiper('.reviews-slider', {
+let hotelSlider = new Swiper('.hotel-slider', {
   // Optional parameters
   loop: true,
 
   // Navigation arrows
   navigation: {
-    nextEl: '.reviews-slider__button--prev',
-    prevEl: '.reviews-slider__button--next',
+    nextEl: '.hotel-slider__button--next',
+    prevEl: '.hotel-slider__button--prev',
   },
+  effect: 'cube',
   keyboard: true,
+});
+let reviewsSlider = new Swiper('.reviews-slider', {
+// Optional parameters
+loop: true,
+
+// Navigation arrows
+navigation: {
+  nextEl: '.reviews-slider__button--prev',
+  prevEl: '.reviews-slider__button--next',
+},
+keyboard: true,
 });
 
 ymaps.ready(init);
-        function init(){
-            var myMap = new ymaps.Map("map", {
-                center: [40.979865, 28.886546],
-                zoom: 15
-    });
-}
-
-var placemark = new ymaps.Placemark([40.979865, 28.886546], {
-    balloonContent: 'Hilton Grand Hotel',
-    hintContent: 'Hilton Grand Hotel' 
+function init(){
+  let myMap = new ymaps.Map("map", {
+              center: [40.979865, 28.886546],
+              zoom: 15
   });
 
-myMap.geoObjects.add(placemark);
-placemark.balloon.open();
+  let placemark = new ymaps.Placemark([40.979865, 28.886546], {
+              balloonContent: 'Hilton Grand Hotel',
+              hintContent: 'Hilton Grand Hotel'
+          });
+
+  myMap.geoObjects.add(placemark);
+  placemark.balloon.open();
+
+}
+
 
 $('.newsletter').parallax({imageSrc: '../img/newsletter-bg.jpg'});
 
 
 var menuButton = document.querySelector(".menu-button");
+console.log(menuButton)
 menuButton.addEventListener("click", function () {
-  console.log("Клик по кнопке меню");
+console.log("Клик по кнопке меню");
+document
+  .querySelector(".navbar-bottom")
+  .classList.toggle("navbar-bottom_visible");
 });
