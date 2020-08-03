7d05caaf0694860356b491a2b3a3e85cb9781bfa
Alise <alicemint@mail.ru>
2020-08-03 13:31:14 +0400

    fix: added esc for modal windiws

--- js/main.js
+++ js/main.js
@@ -32,41 +32,31 @@ $(document).ready(function () {
     $(".navbar-bottom").toggleClass("navbar-bottom_visible");
   });
 
-  let modalButton = $("[data-toggle=modal]");
-  let closeModalButton = $(".modal__close");
-  modalButton.on("click", openModal);
-  closeModalButton.on("click", closeModal);
+  $(document).ready(function () {
+    const modalButton = $("[data-toggle=modal]");
+    const closeModalButton = $(".modal__close");
+    modalButton.on("click", openModal);
+    closeModalButton.on("click", closeModal);
+    
+    function openModal() {
+        const targetModal = $(this).attr("data-href");
+        $(targetModal).find(".modal__overlay").addClass("modal__overlay--visible");
+        $(targetModal).find(".modal__dialog").addClass("modal__dialog--visible");
+    }
+    function closeModal(event) {
+        event.preventDefault();
+        let modalOverlay = $(".modal__overlay");
+        let modalDialog = $(".modal__dialog");
+        modalOverlay.removeClass("modal__overlay--visible");
+        modalDialog.removeClass("modal__dialog--visible");
+        event.stopPropagation();
+    }
+    document.onkeydown = function (evt) {
+        evt = evt || window.event;
+        if (evt.keyCode === 27) {
+            closeModal(evt)
+        }
+    };
+})
 
-  function openModal() {
-    let targetModal = $(this).attr("data-href");
-    $(targetModal).find(".modal__overlay").addClass("modal__overlay--visible");
-    $(targetModal).find(".modal__dialog").addClass("modal__dialog--visible");    
-  }
-  function closeModal(event) {
-    event.preventDefault();
-    let modalOverlay = $(".modal__overlay");
-    let modalDialog = $(".modal__dialog");
-    modalOverlay.removeClass("modal__overlay--visible");
-    modalDialog.removeClass("modal__dialog--visible");
-  }
-
-  //Это то, что я тут навытворяла...
-  
-  /*$(closeModalButton).click(function() {
-    $(this).parents(modalDialog).fadeOut();
-    return false;
-  });
-
-  $(document).keydown(function(e) {
-		if (e.keyCode === 27) {
-			e.stopPropagation();
-      $('.modal').fadeOut();
-		}
-  });
-  $('.modal').click(function(e) {
-		if ($(e.target).closest('.modal').length == 0) {
-			$(this).fadeOut();					
-		}
-	});*/
-  
 });
